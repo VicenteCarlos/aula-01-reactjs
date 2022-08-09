cf45c0e39e7406e24aaaae09015cbd7c844524a4
vicentecarlos <soniamariadias2012@hotmail.com>
2022-08-08 21:25:18 -0300

    feat: implements tests on app

---
+++ src/tests/teste.spec.ts
@@ -0,0 +1,7 @@
+describe("Testa funçoes de uma calculadora", () => { //Describe: agrupa os testes
+     it("Faz uma soma", () => {
+        const sum = 1 + 1;
+
+        expect(sum).toBe(2)
+     }) //it/test faz o teste acontecer
+})
